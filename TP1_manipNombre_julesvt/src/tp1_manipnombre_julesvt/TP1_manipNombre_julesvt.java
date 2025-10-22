077fcd0eb9768951138da38cfa989b3bc0aa923f
jules vincent taveau <jules.vincent--taveau@epfedu.fr>
2025-10-22 12:10:01 +0200

    tp nombre 22/10/25

---
+++ TP1_manipNombre_julesvt/src/tp1_manipnombre_julesvt/TP1_manipNombre_julesvt.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp1_manipnombre_julesvt;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author jules
+ */
+public class TP1_manipNombre_julesvt {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.print ("Entrez le premier entier");
+        int a = scanner.nextInt();
+        System.out.print("Entrez le deuxième entier : ");
+        int b = scanner.nextInt();
+        System.out.println("\nVous avez saisi : a = " + a + " et b = " + b);
+        int somme = a + b;
+        int difference = a - b;
+        int produit = a * b;
+       
+        
+    }
+    
+    
+}
