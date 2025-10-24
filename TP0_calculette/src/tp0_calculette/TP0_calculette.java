07a8b84de3e5916694a4779c8b52bd36c7c59be7
jules vincent taveau <jules.vincent--taveau@epfedu.fr>
2025-10-24 11:42:07 +0200

    tp nombre 22/10/25

---
+++ TP0_calculette/src/tp0_calculette/TP0_calculette.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp0_calculette;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author jules
+ */
+public class TP0_calculette {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+   
+    int nb ;     
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Please enter the operator: \n 1 add \n 2 substract \n 3 divide \n 4 divide \n 5 modulo " );  
+    nb=sc.nextInt();
+    
+    
+        
+    }
+    
+}
