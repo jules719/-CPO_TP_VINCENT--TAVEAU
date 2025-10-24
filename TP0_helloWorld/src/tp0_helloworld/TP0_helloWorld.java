d381e0ae8282ec216a60d695f35e10168e7004ad
jules vincent taveau <jules.vincent--taveau@epfedu.fr>
2025-10-24 11:41:41 +0200

    tp nombre 22/10/25

---
+++ TP0_helloWorld/src/tp0_helloworld/TP0_helloWorld.java
@@ -0,0 +1,28 @@
+/*tp0_helloword 20/10/2025
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp0_helloworld;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author jules
+ */
+public class TP0_helloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println( " Bonjour " ) ; 
+        System.out.println (" Au revoir ") ; 
+String prenom; 
+Scanner sc ; 
+sc = new Scanner (System.in) ; 
+System.out.println("Quel est votre prénom ?"); 
+prenom = sc.nextLine(); 
+    }
+    
+}
