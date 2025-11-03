79c8f13f44263c2b113faf3bde9c3e15c8a4f123
jules vincent taveau <jules.vincent--taveau@epfedu.fr>
2025-11-03 18:51:15 +0100

    tp nombre 22/10/25

--- TP2_Bieres_VincentTaveau/src/tp2_bieres_vincenttaveau/TP2_Bieres_VincentTaveau.java
+++ TP2_Bieres_VincentTaveau/src/tp2_bieres_vincenttaveau/TP2_Bieres_VincentTaveau.java
@@ -14,7 +14,17 @@ public class TP2_Bieres_VincentTaveau {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+    BouteilleBiere uneBiere = new BouteilleBiere() ;
+    uneBiere.nom = "Cuvée des trolls"; 
+    uneBiere.degreAlcool = 7.0 ; 
+    uneBiere.brasserie = "Dubuisson" ; 
+    uneBiere.Ouverte = false ; 
+    uneBiere.lireEtiquette();  
+    
+    uneBiere.nom = "Leffe" ;     
+    uneBiere.degreAlcool = 7.0 ;
+    uneBiere.brasserie = "Abbaye de Leffe" ; 
+    uneBiere.lireEtiquette(); 
     }
     
 }
