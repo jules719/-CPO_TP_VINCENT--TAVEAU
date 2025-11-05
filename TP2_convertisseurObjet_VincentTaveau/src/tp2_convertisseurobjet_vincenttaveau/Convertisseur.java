b3e6a7cc3dcc7174b804a5a0132cfe99e1e56cd7
jules vincent taveau <jules.vincent--taveau@epfedu.fr>
2025-11-05 12:35:23 +0100

    tp nombre 22/10/25

--- TP2_convertisseurObjet_VincentTaveau/src/tp2_convertisseurobjet_vincenttaveau/Convertisseur.java
+++ TP2_convertisseurObjet_VincentTaveau/src/tp2_convertisseurobjet_vincenttaveau/Convertisseur.java
@@ -17,21 +17,21 @@ package tp2_convertisseurobjet_vincenttaveau;
     public Convertisseur() {
         nbConversions = 0;
     }
-   public double CelciusVersKelvin(double tempCelcius) {
+   public double CelsiusVersKelvin(double tempCelcius) {
         nbConversions++;
         return tempCelcius + 273.15;
     }   
-    public double KelvinVersCelcius(double tempKelvin) {
+    public double KelvinVersCelsius(double tempKelvin) {
         nbConversions++;
         return tempKelvin - 273.15;
     }   
-    public double FahrenheitVersCelcius(double tempFahrenheit) {
+    public double FahrenheitVersCelsius(double tempFahrenheit) {
         nbConversions++;
         return (tempFahrenheit - 32) * 5.0 / 9.0;
     }    
-    public double CelciusVersFahrenheit(double tempCelcius) {
+    public double CelsiusVersFahrenheit(double tempCelsius) {
         nbConversions++;
-        return tempCelcius * 9.0 / 5.0 + 32;
+        return tempCelsius * 9.0 / 5.0 + 32;
     }  
     public double KelvinVersFahrenheit(double tempKelvin) {
         nbConversions++;
--- TP2_convertisseurObjet_VincentTaveau/src/tp2_convertisseurobjet_vincenttaveau/TP2_convertisseurObjet_VincentTaveau.java
+++ TP2_convertisseurObjet_VincentTaveau/src/tp2_convertisseurobjet_vincenttaveau/TP2_convertisseurObjet_VincentTaveau.java
@@ -4,6 +4,8 @@
  */
 package tp2_convertisseurobjet_vincenttaveau;
 
+import java.util.Scanner;
+
 /**
  *
  * @author jules
@@ -13,14 +15,66 @@ public class TP2_convertisseurObjet_VincentTaveau {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-     Convertisseur convertisseur1 = new Convertisseur();
 
-        System.out.println("485C = " + convertisseur1.CelciusVersKelvin(485) + " K");
-        System.out.println("5 K = " + convertisseur1.KelvinVersCelcius(5) + " C");
-        System.out.println("500F = " + convertisseur1.FahrenheitVersCelcius(500) + " C");      
-        
-        System.out.println (convertisseur1) ; 
-    } 
-     
+
+    public static void main(String[] args) {
+
+        Convertisseur convertisseur1 = new Convertisseur();
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Saisissez une valeur : ");
+        double valeur = sc.nextDouble();
+
+        // 2. On affiche le menu
+        System.out.println("Saisissez la conversion que vous souhaitez effectuer :");
+        System.out.println("1 - De Celsius vers Kelvin");
+        System.out.println("2 - De Kelvin vers Celsius");
+        System.out.println("3 - De Fahrenheit vers Celsius");
+        System.out.println("4 - De Celsius vers Fahrenheit");
+        System.out.println("5 - De Fahrenheit vers Kelvin");
+        System.out.println("6 - De Kelvin vers Fahrenheit");
+
+        System.out.print("Choisir : ");
+        int choix = sc.nextInt();
+
+        double resultat;
+        String message;
+
+        switch (choix) {
+            case 1:
+                resultat = convertisseur1.CelsiusVersKelvin(valeur);
+                message = valeur + " C = " + resultat + " K";
+                break;
+            case 2:
+                resultat = convertisseur1.KelvinVersCelsius(valeur);
+                message = valeur + " K = " + resultat + " C";
+                break;
+            case 3:
+                resultat = convertisseur1.FahrenheitVersCelsius(valeur);
+                message = valeur + " °F = " + resultat + " C";
+                break;
+            case 4:
+                resultat = convertisseur1.CelsiusVersFahrenheit(valeur);
+                message = valeur + " C = " + resultat + " °F";
+                break;
+            case 5:
+                resultat = convertisseur1.FahrenheitVersKelvin(valeur);
+                message = valeur + " F = " + resultat + " K";
+                break;
+            case 6:
+                resultat = convertisseur1.KelvinVersFahrenheit(valeur);
+                message = valeur + " K = " + resultat + " F";
+                break;
+            default:
+                message = "Choix invalide !";
+                break;
+        }
+
+        // 5. Affichage du résultat
+        System.out.println("\nRésultat : " + message);
+        System.out.println(convertisseur1);
+
+        sc.close();
+    }
 }
+
