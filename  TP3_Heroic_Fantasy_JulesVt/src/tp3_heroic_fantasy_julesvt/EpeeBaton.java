384f057a4a3335f605d421092d6cbaf951ab3a31
jules vincent taveau <jules.vincent--taveau@epfedu.fr>
2025-11-12 11:16:17 +0100

    tp nombre 22/10/25

---
+++  TP3_Heroic_Fantasy_JulesVt/src/tp3_heroic_fantasy_julesvt/EpeeBaton.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp3_heroic_fantasy_julesvt;
+
+/**
+ *
+ * @author jules
+ */
+public class EpeeBaton {
+    public abstract class Arme {
+    String nom;
+    int niveauAttaque;
+
+    public Arme(String nom, int niveauAttaque) {
+        this.nom = nom;
+        this.niveauAttaque = niveauAttaque;
+    }
+
+    public int getNiveauAttaque() {
+        return niveauAttaque;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    @Override
+    public String toString() {
+        return nom + " (niveau d'attaque : " + niveauAttaque + ")";
+    }
+}
+ 
+    
+}
